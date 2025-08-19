cfe2de94675b15bbeeef2ae0704b80ebd5d46608
Mallick <2420676@cognizant.com>
2025-08-19 09:09:34 +0530

    Initial commit: Spring Boot application with Snyk GitHub Action

---
+++ snyk-demo/src/main/java/com/snyk/demo/snykdemo/SnykDemoApplication.java
@@ -0,0 +1,13 @@
+package com.snyk.demo.snykdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnykDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnykDemoApplication.class, args);
+	}
+
+}
---
+++ snyk-demo/src/test/java/com/snyk/demo/snykdemo/SnykDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snyk.demo.snykdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnykDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
